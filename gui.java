0546e56a5a15efab904ae4f5f0fd524ad5ee32e0
bhighlan <bhighlan@mail.umw.edu>
2022-11-22 11:08:51 -0500

    Added methods to GUI

--- gui.java
+++ gui.java
@@ -1,6 +1,32 @@
 import javax.swing.*;
+import java.awt.event.*;
+
 public class gui {
     private boolean continousScrolling;
+
+    public class ButtonListener implements ActionListener(){
+        public void actionPerformed (Action Event e){
+            JOptionPane.showMessageDialog(null, "You pushed the button!");
+        }
+    }
+
+    public void addButton(String t, JFrame f){
+        JButton button = new JButton(text);
+        button.addActionListener(new ButtonListener(text));
+        f.getContentPane().add(button);
+    }
+
+    public void closeWindow(){
+
+    }
+
+    public void displayWindow(String novelContent){
+
+    }
+
+    public void navigationBar(){
+
+    }
 
 
 }
