a8e80c71933f18a3eab78ffc7d6adcab06987f05
tosh1100 <ted@tedstanton.com>
2022-11-27 18:05:49 -0500

    Mostly finished GUI. Takes a hashmap of chapter names mapped to chapter text, a boolean to tell you whether you're in continuous scrolling or not and a String for the novel name. Still needs to know what methods get called when the gui is closed, needs work on the next/previous buttons in chapter selection, and style work

--- gui.java
+++ gui.java
@@ -1,31 +1,78 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class GUI {
+    //creates the GUI, taking a hashmap of chapter names mapped to content
+    //a boolean to tell you whether you're in continuous scrolling or not
+    //and a String for the novel name
     private boolean continousScrolling;
-    private HashMap<String, String> novel;
+    private HashMap novel;
 
-    public GUI(HashMap novel, boolean continousScrolling) {
-        novel = this.novel;
-        continousScrolling = this.continousScrolling;
+    private String novelName;
+
+    public GUI(HashMap novel, String novelName, boolean continousScrolling) {
+        this.novel=novel;
+        this.continousScrolling=continousScrolling;
+        this.novelName=novelName;
+    }
+    public void read(){
+        //Calls the appropriate method depending on whether or not we're in continuous scrolling mode
+        if(continousScrolling){
+            String entireNovel = "";
+            for (Object s: novel.keySet()){
+                entireNovel= entireNovel+ "\n\n" + (String)s + "\n\n" + novel.get(s);
+            }
+            displayWindow(novelName, entireNovel);
+        }else{
+            chapterSelection();
+        }
     }
 
+    public void chapterSelection(){
+        //Creates a ComboBox and a button, when the button is pressed closes this frame and goes to the chapter indicated in the window
+        JFrame f = new JFrame("Select a chapter");
+        f.addWindowListener(new WindowHandler(f));
+        JComboBox cb = new JComboBox<>(novel.keySet().toArray());
+        JButton b = new JButton(new AbstractAction("Read on!") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String chosenKey = (String) cb.getSelectedItem();
+                String title = novelName + ": " + (String) chosenKey;
+                displayWindow(title, (String) novel.get(chosenKey));
+                f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
+            }
+        });
+        JPanel p = new JPanel();
+        p.add(cb);
+        p.add(b);
+        f.add(p);
+        f.setSize(600, 600);
+        f.setVisible(true);
+    }
+    public void addButtons(JPanel p, JFrame f) {
+        //adds buttons to panel, only if not in continuous scrolling
 
-    public void addButtons(JPanel p) {
         if (continousScrolling == false) {
+
             JButton nButton = new JButton(new AbstractAction("Next") {
+                //moves us to next chapter
                 @Override
                 public void actionPerformed(ActionEvent e) {
+                    //needs to open the next chapter using the displaywindow method
+                    f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
 
                 }
             });
 
             JButton pButton = new JButton(new AbstractAction("Prev") {
+                //moves us to previous chapter
                 @Override
                 public void actionPerformed(ActionEvent e) {
-
+                    //needs to go to the prev. chapter using the displaywindow method
+                    f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
                 }
             });
             nButton.setBounds(0, 0, 95, 30);
@@ -35,14 +82,15 @@ public class GUI {
         }
     }
 
-    public void displayWindow() {
-        JFrame f = new JFrame("Reader");
+    public void displayWindow(String title, String text) {
+        //Creates the reader window with the selected novel/chapter and a scroll bar
+        JFrame f = new JFrame(title);
         JPanel p = new JPanel();
         p.setBounds(0, 500, 600, 100);
         p.setBackground(Color.gray);
-        addButtons(p);
+        addButtons(p,f);
         JTextPane tp = new JTextPane();
-        tp.setText("");
+        tp.setText(text);
         tp.setEditable(false);
         JScrollPane sta = new JScrollPane(tp);
         sta.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
@@ -56,14 +104,14 @@ public class GUI {
     }
 }
 class WindowHandler implements WindowListener {
+    //Returns to main menu and sets completion status
     private JFrame frame;
 
     public WindowHandler(JFrame frame) {
         this.frame = frame;
     }
-
+    @Override
     public void windowClosing(WindowEvent e) {
-        //getScrollPosition()
         // Whatever method(s) set the completion status
         // Whatever method gives us the main menu
     }
