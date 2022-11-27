d90e577f1492ffbc596a9dd32d8383d342b6e67f
tosh1100 <ted@tedstanton.com>
2022-11-27 14:25:49 -0500

    basic outline of gui

--- gui.java
+++ gui.java
@@ -1,34 +1,77 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
+import java.util.HashMap;
 
 public class GUI {
     private boolean continousScrolling;
+    private HashMap<String, String> novel;
 
-    public class ButtonListener implements ActionListener(){
-        public void actionPerformed (Action Event e){
-            JOptionPane.showMessageDialog(null, "You pushed the button!");
-        }
+    public GUI(HashMap novel, boolean continousScrolling) {
+        novel = this.novel;
+        continousScrolling = this.continousScrolling;
     }
 
-    public void addButton(String t, JFrame f) {
+
+    public void addButtons(JPanel p) {
         if (continousScrolling == false) {
-            JButton button = new JButton(text);
-            button.addActionListener(new ButtonListener(text));
-            f.getContentPane().add(button);
+            JButton nButton = new JButton(new AbstractAction("Next") {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                }
+            });
+
+            JButton pButton = new JButton(new AbstractAction("Prev") {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                }
+            });
+            nButton.setBounds(0, 0, 95, 30);
+            pButton.setBounds(200, 200, 95, 30);
+            p.add(pButton);
+            p.add(nButton);
         }
     }
-    public void closeWindow(){
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    public void displayWindow() {
+        JFrame f = new JFrame("Reader");
+        JPanel p = new JPanel();
+        p.setBounds(0, 500, 600, 100);
+        p.setBackground(Color.gray);
+        addButtons(p);
+        JTextPane tp = new JTextPane();
+        tp.setText("");
+        tp.setEditable(false);
+        JScrollPane sta = new JScrollPane(tp);
+        sta.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        p.add(sta);
+        f.add(p);
+        f.addWindowListener(new WindowHandler(f));
+
+        f.setSize(600, 600);
+        f.setVisible(true);
+
     }
-
-    public void displayWindow(String novelContent){
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    public void navigationBar(){
-
-    }
-
-
 }
+class WindowHandler implements WindowListener {
+    private JFrame frame;
+
+    public WindowHandler(JFrame frame) {
+        this.frame = frame;
+    }
+
+    public void windowClosing(WindowEvent e) {
+        //getScrollPosition()
+        // Whatever method(s) set the completion status
+        // Whatever method gives us the main menu
+    }
+    public void windowOpened(WindowEvent e) {}
+    public void windowClosed(WindowEvent e) {}
+    public void windowIconified(WindowEvent e) {}
+    public void windowDeiconified(WindowEvent e) {}
+    public void windowActivated(WindowEvent e) {}
+    public void windowDeactivated(WindowEvent e) {}
+}
+
